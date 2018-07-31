142e920dc7860585e4295f7722dfa57672d9de58
Kento75 <kento2github@gmail.com>
2018-07-31 21:38:50 +0900

    add: other modules

---
+++ sec2/condition.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ループ用のカウンタ
+	i := 0
+
+	// for文(while文相当)
+	for i < 5 {
+		fmt.Println(i)
+		i++
+	}
+}
---
+++ sec2/export.go
@@ -0,0 +1,12 @@
+package main
+
+// 先頭が大文字(E)のためエクスポートされる
+const Export = true
+
+// 先頭が小文字(e)のためエクスポートできない
+const export = false
+
+func main() {
+	// 先頭が大文字でもローカル変数のためエクスポートされない
+	const Z = 123
+}
