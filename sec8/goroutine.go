503259110a920cf69833abaa7487912f35261641
Kento75 <kento2github@gmail.com>
2018-08-12 09:21:05 +0900

    Add sec8

---
+++ sec8/goroutine.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("main：開始")
+
+	fmt.Println("testを通常の関数として起動")
+	test()
+
+	fmt.Println("testをゴルーチンとして起動")
+	go test()
+
+	// 3秒待つ
+	time.Sleep(3 * time.Second)
+	fmt.Println("main：終了")
+}
+
+// ゴルーチンとして起動する関数
+// main関数が3秒しか待たないので go test() は途中で終了する
+// チャネルを使用するとごルーチンの終了を待つ設定ができる
+func test() {
+	for i:= 0; i < 5; i++ {
+		// 連番を出力
+		fmt.Println(i)
+
+		// 1秒待つ
+		time.Sleep(1 * time.Second)
+	}
+}
